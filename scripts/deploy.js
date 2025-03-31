fb18c1e726e858af68890de5685d5595a2dae5d2
Dara Heaphy <23369914@studentmail.ul.ie>
2025-03-31 11:58:10 +0100

    ticket token creation works, balance checks working

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,21 @@
-'use strict';
-
 const { ethers } = require("hardhat");
-require("dotenv").config({ path: "./.env" });
+require("dotenv").config();
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
-  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Deploying contracts with the account:", (await ethers.getSigners())[0].address);
   console.log("Vendor Address from .env:", process.env.VENDOR_ADDRESS);
 
   const TicketToken = await ethers.getContractFactory("TicketToken");
   const ticketPrice = ethers.parseEther("0.01");
   const vendorAddress = process.env.VENDOR_ADDRESS;
 
+  // Deploy the contract
   const ticketToken = await TicketToken.deploy(ticketPrice, vendorAddress);
+
+  // Log the deployment transaction hash for debugging
+  console.log("Deployment transaction hash:", ticketToken.deploymentTransaction.hash);
+
+  // Wait for the contract to be mined
   await ticketToken.waitForDeployment();
 
   console.log("TicketToken deployed to:", ticketToken.target);
